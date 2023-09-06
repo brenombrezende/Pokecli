e6200bbe85998e1ec01090bc3ff05477a527ea53
brenombrezende <brenombrezende@gmail.com>
2023-09-06 15:50:27 -0300

    feat / added the Clear command and improved code readability

--- command_help.go
+++ command_help.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func commandHelp() {
-	fmt.Println("Some help here")
+	allCommands := getCommands()
+	fmt.Println("----------")
+	fmt.Println("List of all available commands:")
+	for _, c := range allCommands {
+		fmt.Println(c.name, " - ", c.description)
+	}
+	fmt.Println("----------")
 }
